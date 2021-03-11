b8d39a7ca1c629282815f85ff67a348c6c6b312e
Feike Falkena <feike@feikefalkena.nl>
2021-03-11 17:18:49 +0100

    Dashboard layout

---
+++ pages/team/1E/dashboard.php
@@ -0,0 +1,161 @@
+<div class="containerDashboard" id="block">
+    <div class="pageBar">
+        <div>
+            <h2 id="titel">Dashboard</h2>
+            <p id="desc">Groep INF1E</p>
+        </div>
+        <a class="logout button" href="#">Log uit</a>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6 block" id="block1">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Controle</h5>
+            </div>
+        </div>
+        <div class="col-lg-3 block" id="block2">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Commands</h5>
+            </div>
+        </div>
+        <div class="col-lg-3 block" id="block3">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Stand</h5>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 block" id="block4">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Huidige game</h5>
+            </div>
+        </div>
+        <div class="col-lg-6 block" id="block5">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Speelschema</h5>
+                <!-- Display afhankelijk van spel -->
+                <table id="schemaSPS">
+                    <tr>
+                        <th>Spel</th>
+                        <th>Spelers</th>
+                        <th>Tijd</th>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                    <tr>
+                        <td>SPS</td>
+                        <td>Robot A vs Robot E</td>
+                        <td>13:05</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 block" id="block6">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Historie</h5>
+            </div>
+        </div>
+        <div class="col-lg-6 block" id="block7">
+            <div class="neonBlock content">
+                <h5 class="blockTitle">Console</h5>
+                <div id="console">
+                    <p class="blink">></p>
+                    <p class="error">> Error excecuting function; line 34 invalid syntax</p>
+                    <p>> Excecute function followLine();</p>
+                    <p class="correct">> Connected to server as ROBOT#1323</p>
+                    <p>> Connecting to server</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
