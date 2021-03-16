f0ded8c06b963326a4308b2539e5b534b5888731
Tamara <tamarakijkindevegte@live.nl>
2021-03-16 15:49:49 +0100

    Initial layout gamerules
    Made the first layout of the gamerules. Still work in progress.

--- pages/guest/gameRules.php
+++ pages/guest/gameRules.php
@@ -1,8 +1,56 @@
 <div class="gameRulesContainer">
-	<div class="gameRulesImageContainer">
-		
+	<div class="gameTitle row col-lg-12">
+		<h4>Race</h4>
 	</div>
-	<div class="gameRulesExplanation neonBlock">
+
+	<div class="gameRulesSection row">
+		<div class="gameRulesImageContainer col-lg-6 col-xs-12">
 		
+			<img src="images/guest/Kerst.png" class="gameRulesImage">
+		</div>
+
+		<div class="gameRulesExplanation col-lg-6 col-xs-12">
+			<div class="neon">
+				<div class="neonBlock">
+					<div class="gameRules">
+						<h4>Spelregels</h4>
+						<p>
+							Robots gaan één voor één racen over een lijn en de robot die het parcours als snelste aflegt is de winnaar. De robot mag niet van de lijn afraken. Als dit wel gebeurt is de betreffende robot gediskwalificeerd. De eindstand wordt bepaalt op tijd. Max tijd bedraagt 5 minuten. 
+						</p> 
+						<ul>
+							<li>De race bevat geen bochten van 90 graden. </li>
+							<li>De race wordt gestart via de server. </li>
+							<li>De race stopt bij een door getrokken lijn.(zodat beide IR sensoren tegelijk een zwarte lijn detecteren)</li>
+							<li>Robot geheel buiten de lijn is diskwalificatie. </li>
+							<li>Indien geen enkele robot de eindstreep haalt wint de robot die het verst op het parcours is gekomen zonder van de lijn te komen en de daarop volgende is nr 2 enz.</li> 
+						</ul>
+						<p>Scores:</p>
+						<table>
+							<tr>
+								<td>1ste</td>
+								<td>10 punten</td>
+							</tr>
+							<tr>
+								<td>2de</td>
+								<td>8 punten</td>
+							</tr>
+							<tr>
+								<td>3de</td>
+								<td>6 punten</td>
+							</tr>
+							<tr>
+								<td>4de</td>
+								<td>4 punten</td>
+							</tr>
+							<tr>
+								<td>5de</td>
+								<td>2 punten</td>
+							</tr>
+						</table>
+						<p>Geen Tijd(gediskwalificeerd) 0 punten</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
