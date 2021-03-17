aca62b39a7deaa1d2b327a59a5767186c67d5a7a
Feike Falkena <feike@feikefalkena.nl>
2021-03-17 12:44:56 +0100

    Responsive teams pagina + animaties
    Co-Authored-By: hylkesybesma <73107145+hylkesybesma@users.noreply.github.com>

--- lang/lang.en.php
+++ lang/lang.en.php
@@ -3,5 +3,8 @@
 // If you want to add any new translations,do this in aplhabetical order please 
 return [
 	'HOME' => 'Home',
+	'TEAMS_DESC' => 'Lookup all information avout the competitors in the challenges. Or take a look at the livestream!',
+	'TEAMS_INTRODUCING' => 'Introducing to you:',
+	'TEAMS_TITLE' => 'All Teams'
 	]
 ?>
