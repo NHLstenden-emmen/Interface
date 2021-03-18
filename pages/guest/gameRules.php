9f1b631f308c0dc57a9c164ae205b8fe46235a3d
Tamara <tamarakijkindevegte@live.nl>
2021-03-18 09:58:43 +0100

    Added gamerules content
    Added the game rules, Steen, Papier, Schaar is still missing some content. Also the photo's of the games are missing.

--- pages/guest/gameRules.php
+++ pages/guest/gameRules.php
@@ -1,14 +1,11 @@
 <div class="gameRulesContainer">
-	<div class="gameTitle row col-lg-12">
-		<h4>Race</h4>
-	</div>
-
 	<div class="gameRulesSection row">
-		<div class="gameRulesImageContainer col-lg-6 col-xs-12">
-		
-			<img src="images/guest/Kerst.png" class="gameRulesImage">
+		<div class="gameTitle row col-lg-12">
+			<h4>Race</h4>
 		</div>
-
+		<div class="gameRulesImageContainer col-lg-6 col-xs-12">
+			<img src="" class="gameRulesImage">
+		</div>
 		<div class="gameRulesExplanation col-lg-6 col-xs-12">
 			<div class="neon">
 				<div class="neonBlock">
@@ -27,27 +24,181 @@
 						<p>Scores:</p>
 						<table>
 							<tr>
-								<td>1ste</td>
+								<td>1e plaats:</td>
 								<td>10 punten</td>
 							</tr>
 							<tr>
-								<td>2de</td>
+								<td>2e plaats:</td>
 								<td>8 punten</td>
 							</tr>
 							<tr>
-								<td>3de</td>
+								<td>3e plaats:</td>
 								<td>6 punten</td>
 							</tr>
 							<tr>
-								<td>4de</td>
+								<td>4e plaats:</td>
 								<td>4 punten</td>
 							</tr>
 							<tr>
-								<td>5de</td>
+								<td>5e plaats:</td>
 								<td>2 punten</td>
 							</tr>
+							<tr>
+								<td>Geen tijd:</td>
+								<td>0 punten</td>
+							</tr>
 						</table>
-						<p>Geen Tijd(gediskwalificeerd) 0 punten</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="gameRulesSection row">
+		<div class="gameTitle row col-lg-12">
+			<h4>Doolhof</h4>
+		</div>
+		<div class="gameRulesImageContainer col-lg-6 col-xs-12">
+			<img src="" class="gameRulesImage">
+		</div>
+		<div class="gameRulesExplanation col-lg-6 col-xs-12">
+			<div class="neon">
+				<div class="neonBlock">
+					<div class="gameRules">
+						<h4>Spelregels</h4>
+						<ul>
+							<li>Wanneer de “Start” commando wordt verstuurd naar de robot moet er een timer van start gaan die pas eindigt wanneer de robot bij de finish is. De tijd moet worden verstuurd naar de server en moet zichtbaar zijn in de interface.</li>
+							<li>De robot begint in het midden van de doolhof (te zien op de plattegrond).</li>
+							<li>Als de robot geheel van het pad is dan moet er een “Stop” commando worden verstuurd en wordt er voor de robot geen tijd geregistreerd.</li>
+							<li>Robot mag met gedeeltes over de zwarte lijnen komen omdat de robot niet aan elke kant sensoren heeft waardoor hij het niet kan waarnemen. </li>
+							<li>5 minuten de tijd om uit de doolhof te komen, als de robot het niet haalt binnen 5 min wordt er een “Stop” commando verstuurd naar de robot en wordt er geen tijd geregistreerd.</li>
+							<li>Bij de “Finish” staat een obstakel (muur) die moet worden gedetecteerd door de ultrasonicsensor, als de robot er 5 cm van is stopt hij en wordt de tijd verstuurd naar de server.</li> 
+						</ul>
+						<p>Scores:</p>
+						<table>
+							<tr>
+								<td>1e plaats:</td>
+								<td>10 punten</td>
+							</tr>
+							<tr>
+								<td>2e plaats:</td>
+								<td>8 punten</td>
+							</tr>
+							<tr>
+								<td>3e plaats:</td>
+								<td>6 punten</td>
+							</tr>
+							<tr>
+								<td>4e plaats:</td>
+								<td>4 punten</td>
+							</tr>
+							<tr>
+								<td>5e plaats:</td>
+								<td>2 punten</td>
+							</tr>
+							<tr>
+								<td>Geen tijd:</td>
+								<td>0 punten</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="gameRulesSection row">
+		<div class="gameTitle row col-lg-12">
+			<h4>Steen, Papier, Schaar</h4>
+		</div>
+		<div class="gameRulesImageContainer col-lg-6 col-xs-12">
+			<img src="" class="gameRulesImage">
+		</div>
+		<div class="gameRulesExplanation col-lg-6 col-xs-12">
+			<div class="neon">
+				<div class="neonBlock">
+					<div class="gameRules">
+						<h4>Spelregels</h4>
+						<p>Iedereen speelt tegen iedereen een spel. Aan de hand van het aantal potjes gewonnen stijg je in de poule ranglijst.</p>
+						<ul>
+							<li>Wanneer de “Start” commando wordt verstuurd naar de robot moet er een timer van start gaan die pas eindigt wanneer de robot bij de finish is. De tijd moet worden verstuurd naar de server en moet zichtbaar zijn in de interface.</li>
+							<li>De robot begint in het midden van de doolhof (te zien op de plattegrond).</li>
+							<li>Als de robot geheel van het pad is dan moet er een “Stop” commando worden verstuurd en wordt er voor de robot geen tijd geregistreerd.</li>
+							<li>Robot mag met gedeeltes over de zwarte lijnen komen omdat de robot niet aan elke kant sensoren heeft waardoor hij het niet kan waarnemen. </li>
+							<li>5 minuten de tijd om uit de doolhof te komen, als de robot het niet haalt binnen 5 min wordt er een “Stop” commando verstuurd naar de robot en wordt er geen tijd geregistreerd.</li>
+							<li>Bij de “Finish” staat een obstakel (muur) die moet worden gedetecteerd door de ultrasonicsensor, als de robot er 5 cm van is stopt hij en wordt de tijd verstuurd naar de server.</li> 
+						</ul>
+						<p>Scores:</p>
+						<table>
+							<tr>
+								<td>1e plaats:</td>
+								<td>10 punten</td>
+							</tr>
+							<tr>
+								<td>2e plaats:</td>
+								<td>8 punten</td>
+							</tr>
+							<tr>
+								<td>3e plaats:</td>
+								<td>6 punten</td>
+							</tr>
+							<tr>
+								<td>4e plaats:</td>
+								<td>4 punten</td>
+							</tr>
+							<tr>
+								<td>5e plaats:</td>
+								<td>2 punten</td>
+							</tr>
+							<tr>
+								<td>Geen tijd:</td>
+								<td>0 punten</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="gameRulesSection row">
+		<div class="gameTitle row col-lg-12">
+			<h4>Tekening</h4>
+		</div>
+		<div class="gameRulesImageContainer col-lg-6 col-xs-12">
+			<img src="" class="gameRulesImage">
+		</div>
+		<div class="gameRulesExplanation col-lg-6 col-xs-12">
+			<div class="neon">
+				<div class="neonBlock">
+					<div class="gameRules">
+						<h4>Spelregels</h4>
+						<p>De tekening challenge is een spel waarbij de robot een tekening maakt met een bepaald thema. De robot krijgt een canvas waar hij in moet blijven. Voor meer dan 10 seconden buiten het canvas gaan betekent dan ook een ongeldige tekening waarbij geen score kan worden toegekend. De winnaar zal worden gekozen door middel van een poll die achteraf wordt gerouleerd tussen de tekeningen waarbij alleen ingelogde gebruikers hun stem kunnen laten gelden.</p>
+						<p>* Bij gelijk gemiddelde wordt gekeken welk team als eerste klaar is</p>
+						<p>Scores:</p>
+						<table>
+							<tr>
+								<td>1e plaats:</td>
+								<td>10 punten</td>
+							</tr>
+							<tr>
+								<td>2e plaats:</td>
+								<td>8 punten</td>
+							</tr>
+							<tr>
+								<td>3e plaats:</td>
+								<td>6 punten</td>
+							</tr>
+							<tr>
+								<td>4e plaats:</td>
+								<td>4 punten</td>
+							</tr>
+							<tr>
+								<td>5e plaats:</td>
+								<td>2 punten</td>
+							</tr>
+							<tr>
+								<td>Geen geldige tekening:</td>
+								<td>0 punten</td>
+							</tr>
+						</table>
 					</div>
 				</div>
 			</div>
