6bf83bfc1e44736fa850be5949decc967edfbfe0
Feike Falkena <feike@feikefalkena.nl>
2021-03-19 00:46:57 +0100

    html tags uit navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,34 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-	
-	<!-- Stylesheets -->
-	
-	<link rel="stylesheet" href="css/main/navbar.css">
-	<link rel="stylesheet" href="css/main/main.css">
-	</head>
-	
-	<body>
-		<nav>
-			<div class="nav1">
-				<ul class="navul">
-					<li class="navli"><a href="index.php">Home</a></li>
-					<li class="navli"><a href="#">Teams</a></li>
-					<li class="navli"><a href="#">Live</a></li>
-					<li class="navli"><a href="#">Games</a></li>
-				</ul>
-			</div>
-		
-			<div class="nav2">
-				<ul class="navul">
-					<li class="navli"><a href="index.php">Language EN / NL</a></li>
-					<li class="navli"><a href="#">Log Uit</a></li>
-				</ul>
-			</div>
-		</nav>
-		
-		
-	
-	
-	</body>
-	
+<nav>
+    <div class="nav1">
+        <ul class="navul">
+            <li class="navli"><a href="index.php">Home</a></li>
+            <li class="navli"><a href="#">Teams</a></li>
+            <li class="navli"><a href="#">Live</a></li>
+            <li class="navli"><a href="#">Games</a></li>
+        </ul>
+    </div>
+    <div class="nav2">
+        <ul class="navul">
+            <li class="navli"><a href="index.php">Language EN / NL</a></li>
+            <li class="navli"><a href="#">Log Uit</a></li>
+        </ul>
+    </div>
+</nav>
