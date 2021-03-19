84b7a3dca4795531670db2da205e5a3132002cb9
Tamme12 <Wintam@live.nl>
2021-03-19 12:17:33 +0100

    Update dashboard.php

--- pages/team/1E/dashboard.php
+++ pages/team/1E/dashboard.php
@@ -28,17 +28,17 @@ echo '<link rel="stylesheet" href="css/pages/team/1E/background.css" />';
                     </div>
                     <div class="col-lg-6" id="WASD">
                         <!-- Hiertussen komen de WASD knoppen -->
-                        <div class="wasd">
-                          <div class="row-one">
-                            <button>W</button>
-                          </div>
-                          <div class="row-two">
-                            <button>A</button>
-                            <button>S</button>
-                            <button>D</button>
-                          </div>
+                      <div class="wasd">
+                        <div class="row-one">
+                          <button>W</button>
+                        </div>
+                        <div class="row-two">
+                          <button>A</button>
+                          <button>S</button>
+                          <button>D</button>
                         </div>
                       </div>
+                    </div>
                         <!-- Hiertussen komen de WASD knoppen -->
                     </div>
                 </div>
