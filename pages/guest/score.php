bf8730212585db83eaa56a766de5a4bd888d49dd
Nathan511 <72741392+Nathan511@users.noreply.github.com>
2021-03-19 10:15:24 +0100

    kolommen aangepast

--- pages/guest/score.php
+++ pages/guest/score.php
@@ -7,7 +7,6 @@
             <thead>
                 <tr>
                     <th>Spelnaam</th>
-                    <th>Tijdstip</th>
                     <th>Score</th>
                     <th>Gespeeld door</th>
                 </tr>
@@ -15,73 +14,61 @@
             <tbody>
                 <tr>
                     <td>Doolhof</td>
-                    <td>12:15</td>
                     <td>00:14:56</td>
                     <td>Team A</td>
                 </tr>
                 <tr>
                     <td>Tekenen</td>
-                    <td>12:25</td>
                     <td>8</td>
                     <td>Team B</td>
                 </tr>
                 <tr>
                     <td>Steen, Papier, Schaar</td>
-                    <td>12:30</td>
                     <td>3-2</td>
-                    <td>Team C</td>
+                    <td>Team C - Team E</td>
                 </tr>
                 <tr>
                     <td>Race</td>
-                    <td>12:35</td>
                     <td>00:14:35</td>
                     <td>Team D</td>
                 </tr>
                 <tr>
                     <td>Doolhof</td>
-                    <td>12:45</td>
                     <td>00:10:56</td>
                     <td>Team E</td>
                 </tr>
                 <tr>
                     <td>Race</td>
-                    <td>12:48</td>
                     <td>00:14:35</td>
                     <td>Team B</td>
                 </tr>
                 <tr>
                     <td>Steen, Papier, Schaar</td>
-                    <td>12:50</td>
                     <td>2-3</td>
-                    <td>Team A</td>
+                    <td>Team A - Team B</td>
                 </tr>
                 <tr>
                     <td>Tekenen</td>
-                    <td>13:00</td>
                     <td>10</td>
                     <td>Team D</td>
                 </tr>
                 <tr>
                     <td>Tekenen</td>
-                    <td>13:00</td>
                     <td>10</td>
                     <td>Team D</td>
                 </tr>
                 <tr>
                     <td>Tekenen</td>
-                    <td>13:00</td>
                     <td>10</td>
                     <td>Team D</td>
                 </tr>
                 <tr>
                     <td>Tekenen</td>
-                    <td>13:00</td>
                     <td>10</td>
                     <td>Team D</td>
                 </tr>
                 <tr>
                     <td>Tekenen</td>
-                    <td>13:00</td>
                     <td>10</td>
                     <td>Team D</td>
                 </tr>
