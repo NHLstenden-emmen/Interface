a74fa3afc56d85a4940346f60198afccfc18318d
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-03-22 19:31:56 +0100

    design stop button and start of start the game menu

---
+++ pages/team/1B/dashboard.php
@@ -0,0 +1,58 @@
+<div id="panicButton">
+	<div class="bigStop">
+		<a href="#">STOP THE BOT</a>
+		<div class="icon">
+			<!-- <i class="fas fa-times"></i> -->
+			<i class="fas fa-skull-crossbones"></i>
+		</div>
+	</div>
+</div>
+<div id="dashboard">
+	<div class="liveFeed neonBlock1B">
+		<h1>liveFeed</h1>
+	</div>
+	<div class="startGame neonBlock1B">
+		<h1>start the games</h1>
+		<table id="startGameBlock">
+			<tr class="top-line">
+				<th>game name</th>
+				<th>score</th>
+				<th>start</th>
+				<th>Stop</th>
+			</tr>
+			<tr class="side-line">
+				<th>Race:</th>
+				<td>0</td>
+				<td><i class="fas fa-play"></i></td>
+				<td><i class="fas fa-stop"></i></td>
+			</tr>
+			<tr class="side-line">
+				<th>Doolhof:</th>
+				<td>0</td>
+				<td><i class="fas fa-play"></i></td>
+				<td><i class="fas fa-stop"></i></td>
+			</tr>
+			<tr class="side-line">
+				<th>sps:</th>
+				<td>0</td>
+				<td><i class="fas fa-play"></i></td>
+				<td><i class="fas fa-stop"></i></td>
+			</tr>
+			<tr class="side-line">
+				<th>Tekening:</th>
+				<td>0</td>
+				<td><i class="fas fa-play"></i></td>
+				<td><i class="fas fa-stop"></i></td>
+			</tr>
+		</table>
+	</div>
+	<div class="liveChat neonBlock1B">
+		<h1>liveChat</h1>
+	</div>
+	<div class="console neonBlock1B">
+		<h1>console</h1>
+	</div>
+	<div class="huidigeGameData neonBlock1B">
+		<h1>huidigeGameData</h1>
+	</div>
+</div>
