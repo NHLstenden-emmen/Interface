0361961894129f07d7350f80441422b33cce5d09
Feike Falkena <feike@feikefalkena.nl>
2021-03-23 20:57:42 +0100

    Stylesheets INF1E

--- inc/header.php
+++ inc/header.php
@@ -78,6 +78,11 @@
 				{
 					case 'dashboard':
 						echo '<link rel="stylesheet" href="css/team/1E/dashboard.css">';
+						echo '<link rel="stylesheet" href="css/pages/team/1E/dashboard.css" />';
+						echo '<link rel="stylesheet" media="screen and (min-device-width: 1440px)" href="css/pages/team/1E/desktop-style.css" />';
+						echo '<link rel="stylesheet" media="screen and (min-width: 800px) and (max-width: 1439px)" href="css/pages/team/1E/tablet-style.css" />';
+						echo '<link rel="stylesheet" media="screen and (min-width: 100px) and (max-width: 799px)" href="css/pages/team/1E/mobile-style.css" />';
+						echo '<link rel="stylesheet" href="css/pages/team/1E/background.css" />';
 						break;
 				}
 			}
