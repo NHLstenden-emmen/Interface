7674d79d36b99dd8544fc8051deb27d19ac9cd82
MichelObama <72194919+MichelObama@users.noreply.github.com>
2021-03-25 09:55:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,8 @@
 
     // main dependencies
 
-    include 'inc/mysql.php';
 	include 'inc/socket.php';
 	$socket = new Socket(49153, "194.171.181.139");
-    $db = new Database();
 
     include 'inc/select.php';
     include 'inc/header.php';
