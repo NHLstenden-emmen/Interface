9fa1deaa33159eacaea7215fc27bf37c881b9b40
Feike Falkena <feike@feikefalkena.nl>
2021-03-26 18:28:31 +0100

    Teams uit DB

--- tpl/pages/scheme.php
+++ tpl/pages/scheme.php
@@ -9,122 +9,98 @@
                         <tr>
                             <th>{SCHEME_GAMENAME}</th>
                             <th>{SCHEME_PLAYERS}</th>
-                            <th>{SCHEME_TIMELIMIT}</th>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-                        <tr>
-                            <td>Steen, papier, schaar</td>
-                            <td>Groep E, Groep A</td>
-                            <td>5:00 min</td>
-                        </tr>
-
-                    </table>
-                </div>
-            </div>
-
-        </div>
-        <br><br>
-        <h1 class="title">{SCHEME_HISTORY}</h1>
-        <br>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="neonBlock" data-aos="fade-up">
-                    <table>
-                        <tr>
-                            <th>{SCHEME_GAMENAME}</th>
-                            <th>{SCHEME_PLAYERS}</th>
                             <th>Score</th>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>-</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>-</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>-</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-                        <tr>
+                        <tr class="gameRow" id="1">
                             <td>Steen, papier, schaar</td>
                             <td>Groep E, Groep A</td>
                             <td>1-0</td>
                         </tr>
-
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>1-0</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>1-0</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>1-0</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>1-0</td>
+                        </tr>
+                        <tr class="gameRow" id="1">
+                            <td>Steen, papier, schaar</td>
+                            <td>Groep E, Groep A</td>
+                            <td>1-0</td>
+                        </tr>
                     </table>
                 </div>
             </div>
