b752ea706fbf8ef4ebf6e4a04ec7ec55632b9e64
Tamara <tamarakijkindevegte@live.nl>
2021-03-29 14:14:47 +0200

    Changed some styling
    Changed some styling and put the teamsection in the dashboard.php

--- tpl/team/1A/dashboard.php
+++ tpl/team/1A/dashboard.php
@@ -1,4 +1,4 @@
-<div class="containerDashboard" id="block">
+<div class="container" id="block">
     <div class="pageBar">
         <div>
             <h2 id="titel">Dashboard</h2>
@@ -7,37 +7,60 @@
     </div>
 	
 	<div class="row">
-		<div class="col-lg-5 block" id="block1">
+		<div data-aos="fade-right" class="col-lg-6 block dashboardBlock">
 			<div class="neonBlock content">
 				<h3>Team Informatie</h3>
-				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>	
 			</div>
 		</div>
-		<div class="col-lg-5 block" id="block2">
-			<div class="col col-lg-10 block" id="block1">
-				<div class="neonBlock content">
-					<h3>Command</h3>
-					<button id="readyKnop" class="button" type="button" onclick="start()" name="button">Ready</button>
-				</div>
+		<div data-aos="fade-down" class="col-lg-5 block dashboardBlock">
+			<div class="neonBlock content">
+				<h3>Command</h3>
+				<button class="button" type="button" onclick="start()" name="button">Ready</button>
 			</div>
 		</div>
 	</div>
 
 	<div class="row">
-
+		<div data-aos="fade-up" class="col-lg-12 block">
+			<h3>Hier komt het creatief element</h3>
+		</div>
 	</div>
 </div>
-    <!--  
-    <div class="row">
-		<div class="col col-lg-10 block" id="block5">
-			<div class="neonBlock content">
-                
-            </div>
-		</div>
-	</div>         
-    -->
+
+<section data-aos="fade-up" id="teamPeople">
+  <h1>The A-Team</h1>
+  <div class="container" id="team">
+  	<div class="row justify-content-center">
+    	<h2 class="teamPeopleTitle">Projectgroep A</h2>
+	</div>
+    <div class="row text-center">
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <i class="fas fa-laptop"></i>
+            <h3>Interface Design</h3>
+            <p>Hylke Sijbesma</p>
+            <p>Tamara Kijk in de Vegte</p>
+            <p>Sjouke van der Wal</p>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <i class="fas fa-microchip"></i>
+            <h3>Robot</h3>
+            <p>Lucas Haijtink</p>
+            <p>Yaëll Kuipers</p>
+            <p>Jeffrey Roossien</p>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <i class="fas fa-server"></i>
+            <h3>Server</h3>
+            <p>Remco van Oenen</p>
+            <p>Nick Wind</p>
+        </div>
+    </div>
+  </div>
+</section>       
+   
 	<?php
-		include 'element.html';
+		// include 'element.html';
 	?>
 
 <script>
