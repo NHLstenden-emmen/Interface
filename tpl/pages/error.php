5388786b6b40e1df7bfe5b317e84d86d27319a63
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-03-29 19:56:38 +0200

    created the toast

--- tpl/pages/error.php
+++ tpl/pages/error.php
@@ -1,18 +1,19 @@
 <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px">
 	<!-- Position it -->
-	<div style="position: absolute; top: 100px; right: 30px" id="hoi">
+	<div style="position: absolute; top: 0px; right: 30px" id="placeToast">
 	</div>
 </div>
+<button onclick="javascript:Toast('TITEL', 'hallo')">place toast</button>
 
-<button onclick="javascript:hallo()">hoi</button>
 <script>
-	$(document).ready(function () {
-		$(".toast").toast("show");
-	});
-	var i = 0;
 
-	function hallo()
+	var i = 0;
+	var optionsList = { name: "John", age: 31, city: "New York" };
+
+	function Toast(title, message, buttons = optionsList)
 	{
+		var current = new Date();
+
 		let Toast = document.createElement("div");
 		Toast.setAttribute("role", "alert");
 		Toast.setAttribute("aria-live", "assertive");
@@ -20,16 +21,26 @@
 		Toast.setAttribute("class", "toast");
 		Toast.setAttribute("data-autohide", "false");
 		Toast.id = i++;
-
+		// logo of the toast
 		let ToastImage = document.createElement("img");
 		ToastImage.src = "{assetsFolder}/images/logo.png";
+		ToastImage.classList.add("rounded", "mr-2")
+		ToastImage.alt = "Toast logo";
+		
+		// Title of the toast
+		let ToastStrong = document.createElement("strong");
+		ToastStrong.innerHTML = title;
 
-
+		// save the time it was created at
+		let ToastSmall = document.createElement("small");
+		ToastSmall.classList.add("text-muted")
+		ToastSmall.innerHTML = current.getHours()+":"+current.getMinutes()+":"+current.getSeconds();
+		// button
 		let closeButton = document.createElement("button");
 		closeButton.classList.add("ml-2", "mb-1", "close");
 		closeButton.setAttribute("data-dismiss", "toast");
 		closeButton.setAttribute("aria-label", "Close");
-
+		// span of the button
 		let closeButtonSpan = document.createElement("span");
 		closeButtonSpan.innerHTML = "&times;";
 		closeButtonSpan.setAttribute("aria-hidden", "true");
@@ -38,36 +49,40 @@
 
 		let ToastHeader = document.createElement("div");
 		ToastHeader.classList.add("toast-header");
-		ToastHeader.appendChild(ToastImage);
-		ToastHeader.appendChild(closeButton);
 
 		let ToastBody = document.createElement("div");
 		ToastBody.classList.add("toast-body");
+		// check if a button is needed in the toast
 
+		if(buttons != null) {
+			//buttons.
+			for(var option in buttons) {
+				var buttonElement = document.createElement("button");
+				buttonElement.innerHTML = optionsList[option];
+				buttonElement.setAttribute("onclick", "javascript:sendPoll('"+optionsList[option]+"')");
+				ToastBody.appendChild(buttonElement);
+    		}
+
+		} else {
+			ToastBody.appendChild(document.createTextNode(message));
+		}
+
+		// append the childs (the content) to the Toast
+		ToastHeader.appendChild(ToastImage);
+		ToastHeader.appendChild(ToastStrong);
+		ToastHeader.appendChild(ToastSmall);
+		ToastHeader.appendChild(closeButton);
 
 		Toast.appendChild(ToastHeader);
 		Toast.appendChild(ToastBody);
 
-
-		document.querySelector("#hoi").appendChild(Toast);
-
+		// find the placeToast id and appendChild Toast to it
+		document.querySelector("#placeToast").appendChild(Toast);
+		// show the toast with the settings
 		$('#' + Toast.id).toast('show');
-
 		$('#' + Toast.id).on('hidden.bs.toast', function (e) {
 			console.log(e.currentTarget.id);
 		});
 	}
 
 </script>
-
-<!-- <img src="..." class="rounded mr-2" alt="...">
-
-<strong class="mr-auto">Bootstrap</strong>
-
-<small class="text-muted">just now</small>
-
-<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-	
-	<span aria-hidden="true">&times;</span>
-	
-</button> -->
