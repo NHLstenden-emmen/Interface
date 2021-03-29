d1b1558996559d98dd5a67f635e43a21c0353fe2
Nick Wind <32290253+nick90w@users.noreply.github.com>
2021-03-29 11:57:55 +0200

    begin overzetten

---
+++ tpl/team/1A/dashboard.php
@@ -0,0 +1,43 @@
+<div class="containerDashboard" id="block">
+    <div class="pageBar">
+        <div>
+            <h2 id="titel">Dashboard</h2>
+            <p id="desc">Groep INF1A</p>
+        </div>
+        <a class="logout button" href="#">Log uit</a>
+    </div>
+	
+	<div class="row">
+		<div class="col-lg-5 block" id="block1">
+			<div class="neonBlock content">
+			
+			</div>
+		</div>
+		<div class="col-lg-5 block" id="block2">
+		<div class="col col-lg-10 block" id="block1">
+			<div class="neonBlock content">
+			
+			</div>
+		</div>
+	</div>
+	</div>
+
+
+
+	<div class="row">
+
+	</div>
+</div>
+    <!--  
+    <div class="row">
+		<div class="col col-lg-10 block" id="block5">
+			<div class="neonBlock content">
+                
+            </div>
+		</div>
+	</div>         
+    -->
+	<?php
+		include 'element.html';
+	?>
+
