1289a318ea0a8d75c5d14633a5c9ee2ba8a1a55f
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-03-29 21:26:21 +0200

    table responsive

---
+++ tpl/moderator/pages/dashboards/views/dashboards.php
@@ -0,0 +1,17 @@
+<table>
+	<thead>
+		<tr>
+			<th scope="col">Team</th>
+			<th scope="col">Robot Naam</th>
+			<th scope="col">Dashboard</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td data-label="Team">Race</td>
+			<td data-label="Robot Naam">15/04/2020</td>
+			<td data-label="Dashboard"> karel info enzo </td>
+			</td>
+		</tr>
+	</tbody>
+</table>
