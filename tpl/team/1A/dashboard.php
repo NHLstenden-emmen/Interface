4b95e2f22145084df8305ff9cecb97146e44b978
Tamara <tamarakijkindevegte@live.nl>
2021-03-29 13:15:58 +0200

    Changed primary color
    Changed primary color to our team color. Added a button as a ready button. It's not sending signal to server yet.

--- tpl/team/1A/dashboard.php
+++ tpl/team/1A/dashboard.php
@@ -4,25 +4,24 @@
             <h2 id="titel">Dashboard</h2>
             <p id="desc">Groep INF1A</p>
         </div>
-        <a class="logout button" href="#">Log uit</a>
     </div>
 	
 	<div class="row">
 		<div class="col-lg-5 block" id="block1">
 			<div class="neonBlock content">
-			
+				<h3>Team Informatie</h3>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 			</div>
 		</div>
 		<div class="col-lg-5 block" id="block2">
-		<div class="col col-lg-10 block" id="block1">
-			<div class="neonBlock content">
-			
+			<div class="col col-lg-10 block" id="block1">
+				<div class="neonBlock content">
+					<h3>Command</h3>
+					<button id="readyKnop" class="button" type="button" onclick="start()" name="button">Ready</button>
+				</div>
 			</div>
 		</div>
 	</div>
-	</div>
-
-
 
 	<div class="row">
 
@@ -41,3 +40,8 @@
 		include 'element.html';
 	?>
 
+<script>
+	function start() {
+        
+	}
+</script>
