67cf952893ea94b2acb04f739539604bb593a33d
MichelObama <72194919+MichelObama@users.noreply.github.com>
2021-03-31 18:25:49 +0200

    Brokkobot foto Ingevoegd
    Er is nu een prachtige foto van onze ESP robot op het dashboard

--- tpl/team/1C/dashboard.php
+++ tpl/team/1C/dashboard.php
@@ -11,67 +11,15 @@
 			<p id="titleinf1c">Groep INF1C</p>
 		</div>
 
-		<div class="container3delen">
-			<div class="blocklinks neonBorder">
+		<div class="container2delen">
+			<div class="vaklinks neonBorder">
 				<p><h3 class="Ctitels"> Controle </h3></p> 
 					<form method="post">
 						<button class="button" type="submit" name="Ready">Ready!</button>
 					</form>
 			</div>
-			<div class="blockmidden neonBorder">
-					<p><h3 class="Ctitels"> Commands </h3></p>
-					<p>
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-					</p>
-			</div>
-			<div class="blockrechts neonBorder">
-					<p><h3 class="Ctitels"> Stand </h3></p>
-					<p>
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-					</p>
-			</div>
-		</div>
-		</div>
-		<div class="container2delen">
-			<div class="vaklinks neonBorder">
-			<p><h3 class="Ctitels"> Huidige game </h3></p>
-				<p>
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				</p>
-			</div>
 			<div class="vakrechts neonBorder">
-			<p><h3 class="Ctitels"> Spelschema </h3></p>
-				<p>
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				</p>
+				<img class="brokkobot" src="{assetsFolder}/images/team/1C/Brokobot2kopie.png">
+				<img class="brokkobottekst" src="{assetsFolder}/images/team/1C/BrokkobotTekst.png">
 			</div>
-		</div>
-		<div class="container2delen">
-			<div class="vaklinks neonBorder">
-			<p><h3 class="Ctitels"> Historie </h3></p>
-				<p>
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				</p>
-			</div>
-			<div class="vakrechts neonBorder">
-			<p><h3 class="Ctitels"> Console </h3></p>
-				<p>
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				</p>
-			</div>
-		</div>
-		<div>
-			<p></p>
 		</div>
