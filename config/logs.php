1228ea3065b4b2826cbf46e38fe351987a56e9bc
Feike Falkena <feike@feikefalkena.nl>
2021-03-31 22:17:16 +0200

    Debug

--- config/logs.php
+++ config/logs.php
@@ -5,5 +5,4 @@ ini_set('ignore_repeated_errors', TRUE);
 ini_set('display_errors', FALSE); 
 ini_set('log_errors', TRUE);
 ini_set("error_log", "config/php.log");
-error_log("Oracle database not available!", 0);
 ?>
