5adcc92ced6b0caaced837cccce9ba6a426a3bd4
Kevinsmul <smulders.kevin.1999@gmail.com>
2021-04-01 10:21:06 +0200

    IIS TestDag

--- config/logs.php
+++ config/logs.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 ini_set('ignore_repeated_errors', TRUE);
-ini_set('display_errors', FALSE); 
+ini_set('display_errors', TRUE); 
 ini_set('log_errors', TRUE);
 ini_set("error_log", "config/php.log");
 ?>
