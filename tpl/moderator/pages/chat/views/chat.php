1882107585dec2fa1bc1df77a3136a6e72640546
battlebotsserver <81801444+battlebotsserver@users.noreply.github.com>
2021-04-06 23:20:40 +0200

    DB
    Co-Authored-By: Feike Falkena <feike@feikefalkena.nl>

--- tpl/moderator/pages/chat/views/chat.php
+++ tpl/moderator/pages/chat/views/chat.php
@@ -1,3 +1,10 @@
-<table>
+<h4>Woord toevoegen</h4>
+<form method="POST">
+	<input type="text" name="ban_word" placeholder="Ban word" required>
+	<button class="button">Toevoegen</button>
+</form>
+<br>
+<h4>Banned Words</h4>
+<table id="bannedWordList">
 	{wordList}
 </table>
