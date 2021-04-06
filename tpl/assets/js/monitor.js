e1d789ab58cf808fe8860a97d0cae3ba312f6935
battlebotsserver <81801444+battlebotsserver@users.noreply.github.com>
2021-04-06 18:43:51 +0200

    dashboard fix talen
    Co-Authored-By: Feike Falkena <feike@feikefalkena.nl>

--- tpl/assets/js/monitor.js
+++ tpl/assets/js/monitor.js
@@ -185,3 +185,28 @@ function initializeMonitor() {
 		}
 	}
 }
+
+const titlePage = document.title;
+document.addEventListener('visibilitychange', function() {
+	if(document.hidden) {
+		if(readCookie('lang') == 'nl'){
+			document.title = "Hé kom terug!";
+		} else {
+			document.title = "Come back!";
+		}
+	} else {
+		document.title = titlePage;
+	}
+});
+
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
