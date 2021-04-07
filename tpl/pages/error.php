994ba965cee87d16292d8ea42440f3ffb7532c95
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-04-07 10:44:25 +0200

    added a  404 page

---
+++ tpl/pages/error.php
@@ -0,0 +1,17 @@
+<div id="errorPage">
+	<div class="numbers">
+		<div class="number">
+			<div class="four"></div>
+		</div>
+		<div class="number">
+			<div class="zero"><span></span></div>
+		</div>
+		<div class="number">
+			<div class="four last"></div>
+		</div>
+	</div>
+	<div class="text"> 
+		<p>Het lijkt er op alsof je verdwaalt bent.</p>
+		<p>Laten we je terug naar de <a href="start">start</a> brengen.</p>
+	</div>
+</div>
